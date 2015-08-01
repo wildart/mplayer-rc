6a3872aa7836969c9fecd7768a746bc02b704ada
Michael Cross <mc@xi2.org>
2015-08-01 23:08:01 +0100

    Add copyright header to backends.go

--- backends.go
+++ backends.go
@@ -1,3 +1,24 @@
+/*
+   Copyright 2015 The MPlayer-RC Authors. See the AUTHORS file at the
+   top-level directory of this distribution and at
+   <https://xi2.org/x/mplayer-rc/AUTHORS>.
+
+   This file is part of MPlayer-RC.
+
+   MPlayer-RC is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   MPlayer-RC is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MPlayer-RC.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
